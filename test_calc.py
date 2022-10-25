6d7ff0535a8c11a24b06fbc89c98c72c89e69c63
petr skalicky <petrskali@gmail.com>
2022-10-25 20:21:00 +0200

    deleted unuse file

--- test_calc.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import calc
-
-class TestCalc(unittest.TestCase):
-
-    def test_add(self):
-        self.assertEqual(calc.add(1, 2), 1)
-        self.assertEqual(calc.add(-1, 2), -1)
